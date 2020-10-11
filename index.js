1c8a65dff1a76d110c593b7f91c2c65cbd8b3621
callbacc <alexa.matei@icloud.com>
2020-10-11 13:16:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const client = new Client();
 
 client.config = require("./config.js");
 
+client.on('message', message => {
+    message.channel.send("pong")
+})
+
 client.login(client.config.token);
+
+console.log("yeah")
